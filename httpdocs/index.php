b0629395371122c28eb1d7f826bd87eaf31fb086
KylitoH <kyle.hazan@gmail.com>
2017-03-01 13:51:34 +0100

    add google analytics

--- httpdocs/index.php
+++ httpdocs/index.php
@@ -1351,6 +1351,16 @@ Recouvert d'un cuir très souple il s'intégrera parfaitement et offre une assis
     </div>
 </body>
 
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-92800370-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
 <script src="https://npmcdn.com/masonry-layout@4.0/dist/masonry.pkgd.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.18.0/TweenMax.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.5/ScrollMagic.js"></script>
