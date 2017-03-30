d0ac2b8861ec5e947925b390d46a1a9a0cca45ab
KylitoH <kyle.hazan@gmail.com>
2017-03-30 14:25:26 +0200

    payment stripe files

--- stripe_test/payment.php
+++ stripe_test/payment.php
@@ -6,23 +6,15 @@ $email = $_POST['email'];
 
 $name = $_POST['name'];
 
+
 if (filter_var($email, FILTER_VALIDATE_EMAIL) && !empty($name) && !empty($token)) {
-    $ch = curl_init();
-    
-    $data = [
+    require('stripe.php');
+    $stripe = new Stripe('sk_test_51RktZrgW3bG1QPtiO9IP4a3');
+    $customer = $stripe('customers', [
         'source' => $token,
         'description' => $name,
         'email' => $email
-    ];
-    curl_setopt_array($ch, [
-        CURLOPT_URL => 'https://api.stripe.com/v1/customers',
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_USERPWD => 'sk_test_51RktZrgW3bG1QPtiO9IP4a3',
-        CURLOPT_HTTPAUTH => CURLAUTH_BASIC,
-        CURLOPT_POSTFIELDS => http_build_query($data)
-        ]);
-    $response = json_decode(curl_exec($ch));
-    curl_close($ch);
-    var_dump($response);
+    ]);
+    var_dump($customer);
     die();
 }
